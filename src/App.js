0cc1470c1c754f64adfb8961e6f414868ad37ef2
Jason Thulen <74831518+jaysun025@users.noreply.github.com>
2021-04-27 12:48:53 -0400

    added links and AOE Helper Tree

--- src/App.js
+++ src/App.js
@@ -17,23 +17,28 @@ function App() {
           </p>
         </div>
       <h1>Projects</h1>
-      <h2 className = 'evening'>Evening of the Dead</h2>
         <div className = 'projects'>
           <div className = 'zombies'>
+          <h2 className = 'evening'>Evening of the Dead</h2>
             <button className = 'zombtn'>Evening of the Dead</button>
               <div className = 'zomContent'>
                 <p className = 'zombieP'>
                   Using JavaScript, CSS, and HTML I created a dodge the falling 
                   objects game. It has  zombie theme to it and I did the art work
                   myself.
+                  <a href ='https://evening-of-the-dead.netlify.app/' target = '_blank'>See how long you can last.</a>
                 </p>
               </div>
           </div>
           <div className = 'ageOfEmpiresTwo'>
+          <h2 className = 'aoe'>Age of Empires Helper Tree</h2>
             <button className = 'empirebtn'>Age of Empires Helper Tree</button>
             <div className = 'empireContent'>
               <p className = 'empireP'>
-                We Shall Conquor all!!!
+                My first full-stack application using a Heroku API. I wanted to make a help guide so I 
+                could play Age of Empires 2 better. It helps you find your favorite types of units or tech and save
+                them to a favorites list. 
+                <a href = 'https://aoe-favs.herokuapp.com/profile' target = '_blank'>Find your favorites.</a>
               </p>
             </div>
           </div>
